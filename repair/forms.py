8dc030b0725a3f843a4a4fe889ac1e1b7cba251c
SamirAlizada <samir.elizade5555@gmail.com>
2024-07-01 19:34:32 +0400

    added new date_format

--- repair/forms.py
+++ repair/forms.py
@@ -1,14 +1,44 @@
 from django import forms
 from .models import Device, Product, ProductSold, Category
-from datetime import datetime
+from datetime import datetime, date
+
+class CustomDateInput(forms.DateInput):
+    input_type = 'text'
+    format = '%d/%m/%Y'
+
+    def __init__(self, *args, **kwargs):
+        kwargs['format'] = self.format
+        super().__init__(*args, **kwargs)
+
+    def format_value(self, value):
+        if value:
+            if isinstance(value, (datetime, date)):
+                return value.strftime(self.format)
+        return value
 
 class DeviceForm(forms.ModelForm):
+    add_date = forms.CharField(widget=CustomDateInput(attrs={'class': 'form-control', 'placeholder': 'DD/MM/YYYY'}))
+
     class Meta:
         model = Device
         fields = ['device_name', 'customer_name', 'repair_cost', 'repair_duration', 'add_date', 'status', 'notes']
         widgets = {
             'notes': forms.Textarea(attrs={'rows': 4, 'cols': 40, 'style': 'width:100%;'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not self.instance.pk:  # If create new form
+            self.fields['add_date'].initial = date.today().strftime('%d/%m/%Y')
+
+    def clean_add_date(self):
+        add_date = self.cleaned_data['add_date']
+        if isinstance(add_date, str):
+            try:
+                return datetime.strptime(add_date, '%d/%m/%Y').date()
+            except ValueError:
+                raise forms.ValidationError("Enter the date in DD/MM/YYYY format.")
+        return add_date
 
 class CategoryForm(forms.ModelForm):
     class Meta:
@@ -22,6 +52,7 @@ class ProductForm(forms.ModelForm):
 
 class ProductSoldForm(forms.ModelForm):
     category = forms.ModelChoiceField(queryset=Category.objects.all(), required=True)
+    date = forms.CharField(widget=CustomDateInput(attrs={'class': 'form-control', 'placeholder': 'DD/MM/YYYY'}))
 
     class Meta:
         model = ProductSold
@@ -29,6 +60,8 @@ class ProductSoldForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if not self.instance.pk:  # if create new form
+            self.fields['date'].initial = date.today().strftime('%d/%m/%Y')
         if 'category' in self.data:
             try:
                 category_id = int(self.data.get('category'))
@@ -36,9 +69,7 @@ class ProductSoldForm(forms.ModelForm):
             except (ValueError, TypeError):
                 self.fields['product_name'].queryset = Product.objects.none()
         elif self.instance.pk:
-            # Set the initial value for category based on the product's category
             self.fields['category'].initial = self.instance.product_name.category_name
-            # Filter product_name queryset based on the current category
             self.fields['product_name'].queryset = Product.objects.filter(category_name=self.instance.product_name.category_name).order_by('product_name')
         else:
             self.fields['product_name'].queryset = Product.objects.none()
@@ -50,5 +81,14 @@ class ProductSoldForm(forms.ModelForm):
 
         if product and count:
             if product.stock_number < count:
-                raise forms.ValidationError(f"Only {product.stock_number} items available in stock.")
+                raise forms.ValidationError(f"Only {product.stock_number} products are in stock.")
         return cleaned_data
+
+    def clean_date(self):
+        date_str = self.cleaned_data['date']
+        if isinstance(date_str, str):
+            try:
+                return datetime.strptime(date_str, '%d/%m/%Y').date()
+            except ValueError:
+                raise forms.ValidationError("Enter the date in DD/MM/YYYY format.")
+        return date_str
